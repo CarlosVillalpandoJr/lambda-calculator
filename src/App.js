6cb3631d0f8415800d0baa827860ef76d048c626
Carlos Villalpando, Jr <carlosvillalpandojr21@yahoo.com>
2019-10-08 14:55:25 -0500

    Display component showing; also displaying Numbers component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from "react";
 import "./App.css";
+import Display from './components/DisplayComponents/Display'
+import Numbers from './components/ButtonComponents/NumberButtons/Numbers';
 // STEP 4 - import the button and display components
 // Don't forget to import any extra css/scss files you build into the correct component
 
@@ -18,6 +20,8 @@ function App() {
       <Logo />
       <div className="App">
         {/* STEP 4 - Render your components here and be sure to properly import/export all files */}
+        <Display />
+        <Numbers />
       </div>
     </div>
   );
